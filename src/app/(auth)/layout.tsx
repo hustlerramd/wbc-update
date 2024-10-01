700ccc16624fb701ed7446299ffe3c5cac733828
Bhavesh-1803 <hustlers.bhavesh@gmail.com>
2024-10-01 11:10:16 +0530

    Some change for Project run

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,34 +3,48 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import siteConstants from "@/model/constants";
 import dynamic from "next/dynamic";
-const AuthLeftSection = dynamic(() => import("@/modules/auth/components/authLeftSection"), { ssr: false });
+
+const AuthLeftSection = dynamic(
+  () => import("@/modules/auth/components/authLeftSection"),
+  { ssr: false }
+);
 const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata(): Promise<Metadata> {
-	try {
-		const res = await fetch(siteConstants.ENV.SERVER_META_URL + "meta/register");
-		return res.json();
-	} catch (error) {
-		return new Promise<Metadata>(() => ({}));
-	}
-}
-export default async function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<div className="flex flex-row w-screen h-screen m-0 p-0">
-					<div className="flex flex-row w-full">
-						<section className="hidden max-w-[50%] md:w-[50%] md:flex flex-auto ">
-							<AuthLeftSection />
-						</section>
-						<section className="flex flex-auto bg-white w-[100%] md:w-[50%]">{children}</section>
-					</div>
-				</div>
-			</body>
-		</html>
-	);
+// export async function generateMetadata(): Promise<Metadata> {
+//   try {
+//     const res = await fetch(
+//       siteConstants.ENV.SERVER_META_URL + "meta/register"
+//     );
+//     return res.json();
+//   } catch (error) {
+//     return new Promise<Metadata>(() => ({}));
+//   }
+// }
+
+export const metadata: Metadata = {
+  title: "Register | Womens Business Club",
+  description: "Womens Business Club registration page",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-row w-screen h-screen m-0 p-0">
+          <div className="flex flex-row w-full">
+            <section className="hidden max-w-[50%] md:w-[50%] md:flex flex-auto ">
+              <AuthLeftSection />
+            </section>
+            <section className="flex flex-auto bg-white w-[100%] md:w-[50%]">
+              {children}
+            </section>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,29 @@ import "./globals.css";
 import siteConstants from "@/model/constants";
 
 const inter = Inter({ subsets: ["latin"] });
-export async function generateMetadata(): Promise<Metadata> {
-	try {
-		const res = await fetch(siteConstants.ENV.SERVER_META_URL + "meta/lending");
-		return res.json();
-	} catch (error) {
-		return new Promise<Metadata>(() => ({}));
-	}
-}
-export default async function RootLayout({
-	getSSideProps,
-	children,
-}: Readonly<{
-	getSSideProps: Metadata;
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+// export async function generateMetadata(): Promise<Metadata> {
+//   try {
+//     const res = await fetch(siteConstants.ENV.SERVER_META_URL + "meta/lending");
+//     return res.json();
+//   } catch (error) {
+//     return new Promise<Metadata>(() => ({}));
+//   }
+// }
+
+export const metadata: Metadata = {
+  title: "Women's Business Club",
+  description:
+    "Women's Business Club, Empowering ambitious women to succeed. We offer mentoring, networking, and training to businesswomen across the world. We meet for business events in person worldwide.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
